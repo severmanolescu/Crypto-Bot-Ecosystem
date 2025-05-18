ba7f669c9e7a769b301d4c8acda57a49bd7217ec
Sever <severmanolescu1@gmail.com>
2025-05-18 19:21:39 +0300

    Improve the logger

--- sdk/Logger.py
+++ sdk/Logger.py
@@ -1,11 +1,22 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
+
 def setup_logger(log_file):
+    # Create a logger with a specific name
+    logger = logging.getLogger('Crypto-Articles_Bots')
+
+    # Set the level
+    logger.setLevel(logging.INFO)
+
+    # Create the handler
     handler = RotatingFileHandler(log_file, maxBytes=100_000_000, backupCount=3)
-    logging.basicConfig(
-        handlers=[handler],
-        level=logging.INFO,
-        format='%(asctime)s | %(levelname)s | %(name)s | %(message)s'
-    )
-    return logging.getLogger()
+
+    # Create a formatter and set it on the handler
+    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(name)s | %(message)s')
+    handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(handler)
+
+    return logger
