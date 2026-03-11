22dd10cbdce0c6a6a5ab48906db76215740465f2
Sever Manolescu <47499746+severmanolescu@users.noreply.github.com>
2026-03-12 01:23:04 +0200

    Use virtual environment Python to run fan controller

--- scripts/fan_controller/start_controller.sh
+++ scripts/fan_controller/start_controller.sh
@@ -6,5 +6,8 @@ SESSION_NAME="fan_controller"
 # Path to your Python script
 PYTHON_SCRIPT="fan_controller.py"
 
+# Full path to venv Python
+VENV_PYTHON="venv/bin/python"
+
 # Start the screen session and run the Python script
-screen -dmS "$SESSION_NAME" python "$PYTHON_SCRIPT"
+screen -dmS "$SESSION_NAME" "$VENV_PYTHON" "$PYTHON_SCRIPT"
