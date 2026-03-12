c3f0b9ed09737b14fc5927a9562eb7df70ba5507
Sever Manolescu <severmanolescu1@gmail.com>
2026-03-12 14:26:39 +0200

    Log some info when starting the fan controller

--- scripts/fan_controller/start_controller.sh
+++ scripts/fan_controller/start_controller.sh
@@ -11,4 +11,12 @@ VENV_PYTHON="venv/bin/python"
 
 # Start the screen session and run the Python script
 export TERM=xterm
+
+echo "Starting fan controller..."
 screen -dmS "$SESSION_NAME" "$VENV_PYTHON" "$PYTHON_SCRIPT"
+
+if [ $? -eq 0 ]; then
+    echo "Screen session started successfully"
+else
+    echo "Failed to start screen session"
+fi
